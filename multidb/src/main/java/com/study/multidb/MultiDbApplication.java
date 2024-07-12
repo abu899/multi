d1cc542f0736adeb6f16d9f3840ca99edb37a53d
brettahn <abu@successmode.kr>
2024-07-12 14:11:33 +0900

    Initial commit

---
+++ multidb/src/main/java/com/study/multidb/MultiDbApplication.java
@@ -0,0 +1,14 @@
+package com.study.multidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDbApplication.class, args);
+	}
+
+}
+
